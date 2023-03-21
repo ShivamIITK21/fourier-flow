29b56e66ee37d045e9f938503c3d0ec5688ec109
shivam <sshivam21@iitk.ac.in>
2023-03-22 00:04:56 +0530

    updates

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const clear = document.getElementById("clear")
+const submit = document.getElementById("submit")
 
 let isDrawing = false;
 let points = [];
+let hasDrawn = false;
 
 // document.getElementById('canvas').setAttribute("width", document.querySelector('#localVideo').offsetWidth);
 // document.getElementById('canvas').setAttribute("height", document.querySelector('#localVideo').offsetHeigh);
@@ -14,6 +16,10 @@ canvas.addEventListener('mousemove', drawPoint);
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mouseout', stopDrawing);
 
+submit.addEventListener('click', () => {
+  let newPoints = points.map(point => pointCornertoCenter(point))
+  console.log(newPoints)
+})
 
 clear.addEventListener('click', () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -21,6 +27,7 @@ clear.addEventListener('click', () => {
 })
 
 function startDrawing(e) {
+  if (hasDrawn) return;
   console.log("yep")
   isDrawing = true;
   let x = e.offsetX;
@@ -32,12 +39,10 @@ function startDrawing(e) {
 
 function drawPoint(e) {
   console.log("drawing...")
+  if (hasDrawn) return;
   if (!isDrawing) return;
   let x = e.offsetX;
   let y = e.offsetY;
-  console.log(x,y)
-  console.log(canvas.offsetLeft)
-  console.log(canvas.offsetTop)
   points.push({x: x, y: y});
   drawLine(points[points.length-2], points[points.length-1]);
   // drawPointOnce(x,y);
@@ -45,9 +50,17 @@ function drawPoint(e) {
 }
 
 function stopDrawing(e) {
+  if (hasDrawn) return;
   isDrawing = false;
   ctx.lineTo(points[0].x, points[0].y)
   ctx.stroke()
+  if(isPointInside({x:e.offsetX, y:e.offsetY})){
+    let newpoints = getPoints(points[points.length - 1], points[0])
+    console.log(newpoints)
+    points = points.concat(newpoints)
+    console.log(points)
+  }
+  hasDrawn = true;
 }
 
 function drawPointOnce(x, y) {
@@ -66,4 +79,37 @@ function drawLine(p1, p2) {
   ctx.stroke();
 }
 
-// drawLine({x:0,y:0},{x:400, y:400})
+
+//function to get points on a line
+const getPoints = (p1, p2) => {
+  let newpoints = []
+  let slope = (p1.y-p2.y)/(p1.x-p2.x)
+  for(let i = p1.x; i !== p2.x;){
+    let y = Math.round(p1.y + (i-p1.x)*slope) 
+    newpoints.push({x:i, y:y})
+
+    if(i < p2.x){
+      i++;
+    }
+    else{
+      i--;
+    }
+  }
+  return newpoints
+}
+
+const pointCornertoCenter = (point) => {
+  return {x:point.x-400, y:400-point.y}
+}
+
+const pointCentertoCorner = (point) => {
+  return {x:point.x+400, y:point.y-400}
+}
+
+//func to check if pt is inside, wrt corner
+const isPointInside = (point) => {
+  if((point.x > 0 && point.x < 800) && (point.y > 0 && point.y < 800)){
+    return true;
+  }
+  return false;
+}
